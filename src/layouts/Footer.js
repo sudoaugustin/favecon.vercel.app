80c18934e377309b52618004e431d5d891204044
Augustin Joseph <sudoaugustin@gmail.com>
2021-10-27 17:42:03 +0630

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -7,21 +7,12 @@ const Footer = () => (
     {/* <-- Left --> */}
     <div className='inline-flex items-end space-x-1.5'>
       <p className='text-gray-600 text-xs font-semibold leading-none tracking-wide'>
-        A PROJECT AT{' '}
+        A PROJECT BY{' '}
       </p>
-      <a href='https://github.com/sudolab' children={<SudoLabLogo />} />
+      <a href='https://github.com/sudolab' className="font-semibold text-xs text-sky-600 leading-none tracking-wide" >Augustin Joseph</a>
     </div>
     {/* <-- Right --> */}
     <div className='inline-flex space-x-6'>
-      <div className='hidden md:inline-flex items-center space-x-1'>
-        <KhabyIcon className='w-8 inline' />
-        <span>
-          by{' '}
-          <a target='_blank' className='text-sky-400' href='https://dribbble.com/AndriiKovalchuk'>
-            Andrii Kovalchuk
-          </a>
-        </span>
-      </div>
       <a
         className='text-gray-600 hover:text-gray-400 transition duration-300 inline-block'
         href='https://github.com/sudolab/favecon'>
